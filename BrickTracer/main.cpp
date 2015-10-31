40b3c9cc25725a723021ea0791b44fa55e7addce
Kay Rottmann <rottmann@Kays-MacBook-Air.local>
2015-10-31 13:46:19 -0700

    Initial Commit

---
+++ BrickTracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BrickTracer
+//
+//  Created by Kay Rottmann on 31.10.15.
+//  Copyright (c) 2015 Kay Rottmann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
